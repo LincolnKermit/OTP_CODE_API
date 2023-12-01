aaa19466f1e0e152d7f1af7156daa8a82324736d
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2023-12-01 11:00:20 +0100

    FIXED API ! (Headers issues got fixed)

--- otp_main.py
+++ otp_main.py
@@ -83,7 +83,7 @@ os.system("clear")
 print("Country Code... OK")
 print("Header Check...")
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 }
 time.sleep(0.5)
 os.system("clear")
